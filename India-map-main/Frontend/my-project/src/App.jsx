1c4e999561407af9756632f405af32e109df8288
Vedu544 <85640004+Vedu544@users.noreply.github.com>
2025-06-18 21:04:19 +0530

    frontend change

--- India-map-main/Frontend/my-project/src/App.jsx
+++ India-map-main/Frontend/my-project/src/App.jsx
@@ -4,7 +4,7 @@ import axios from "axios"
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 console.log(import.meta.env.VITE_API_BASE_URL)
-axios.defaults.withCredentials = true
+
 function App() {
   return (
     <>
